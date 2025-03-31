4f8e3a70e81a21f8f56b1c9614d3855e1c3dd705
yamasaki <izoulol@gmail.com>
2025-03-31 21:36:09 +0900

    9cc.cの修正

--- 9cc.c
+++ 9cc.c
@@ -5,6 +5,7 @@ int main(int argc, char **argv){
     if (argc != 2) {
         fprintf(stderr, "引数の個数が正しくありません\n");
         return 1;
+    }
 
         char *p = argv[1];
 
@@ -26,8 +27,7 @@ int main(int argc, char **argv){
         }
             fprintf(stderr, "不正な演算子です: '%c'\n", *p);
             return 1;
-        }
+    }
         printf("    ret\n");
         return 0;
-    }
 }
