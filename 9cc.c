270eae44e0906296df5a09c16970a970358e6251
izoulol <izoulol@gmail.com>
2025-04-02 09:03:15 +0900

    トークンの種類とエラーハンドリング機能を追加し、トークンを消費する関数を実装

--- 9cc.c
+++ 9cc.c
@@ -1,6 +1,48 @@
+#include <ctype.h>
+#include <stdarg.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
+// トークンの種類
+typedef enum {
+    TK_RESERVED,    //記号
+    TK_NUM,         //整数トークン
+    TK_EOF,         //入力の終わりを表すトークン
+} TokenKind;
+
+typedef struct Token Token;
+
+//トークン型
+struct Token {
+    TokenKind kind; //トークンの型
+    Token *next;    //次の入力トークン
+    int val;        //kindがTK_NUMの場合、その数値
+    char *str;      //トークン文字列
+};
+
+//現在着目しているトークン
+Token *token;
+
+//エラーを報告するための関数
+//printfと同じ引数を取る
+void error(char *fmt,...){
+    va_list ap;
+    va_start(ap, fmt);
+    vfprintf(stderr, fmt, ap);
+    fprintf(stderr, "\n");
+    exit(1);
+}
+
+//次のトークンが期待しいる記号のときには、トークンを１つ読み進めて
+//真を返す。それ以外の場合には偽を返す。
+bool consume(char op){
+    if(token->kind != TK_RESERVED || token->str[0] != op)
+        return false;
+    token = token -> next;
+    return true;
+}
 int main(int argc, char **argv){
     if (argc != 2) {
         fprintf(stderr, "引数の個数が正しくありません\n");
